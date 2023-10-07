3d70e2c493d9f441a5cc7f827ffedb3937df9225
daydreamer767910 <daydreamer767910@gmail.com>
2023-10-06 23:02:09 -0700

    optimization

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,4 @@
 import * as path from "path";
-import { Command } from "commander";
 import * as APP from "./app";
 import { GetLogger } from "./logger";
 import * as Readline from "readline";
@@ -18,40 +17,6 @@ function cli_startup() {
   const packageInfo = JSON.parse(
     fs.readFileSync(path.resolve(__dirname, "../package.json"), "utf-8")
   );
-  //console.log(packageInfo.version);
-  const program = new Command();
-  program
-    .version(packageInfo.version)
-    .description(packageInfo.description)
-    .option("paidan  [sourceDir,destDir]", "generate the daily bill")
-    .option("huidan [sourceDir,destDir]", "merge the courier bill")
-    .option("filter [filter,sourceDir,destDir]", "merge the filtered bill")
-    .option("client [sourceDir,destDir]", "collect the client info from bill")
-    .parse(process.argv);
-  //console.info(process.argv.slice(2).length);
-  if (process.argv.slice(2).length === 1) {
-    program.outputHelp();
-  } else if (process.argv.slice(2).length >= 3) {
-    const sourceDir = path.resolve(__dirname, process.argv[3]);
-    const destDir = path.resolve(__dirname, process.argv[4]);
-    const options = process.argv[5];
-    const cmd = process.argv[2].toLowerCase();
-    APP.postMessage({ msgType: cmd, opt: [sourceDir, destDir, options] }).then(
-      //APP.OnCommand(cmd as APP.CommandType, sourceDir, destDir, options).then(
-      () => {
-        GetLogger("app").log(
-          "info",
-          "%s done and the program will exit in 5sec",
-          cmd
-        );
-        setTimeout(() => {
-          process.exit(0);
-        }, 5000);
-        //
-      }
-    );
-  }
-
   //cli for dev and test
   const rl = Readline.createInterface({
     input: process.stdin,
@@ -67,7 +32,7 @@ function cli_startup() {
     switch (cmds[0]) {
       case "help":
         console.log(
-          "Available commands: help,prebill,paidan,huidan,test, loop N,send ,exit"
+          "Available commands: help,paidan,huidan,test, loop N,send ,exit"
         );
         break;
       case "exit":
@@ -75,10 +40,13 @@ function cli_startup() {
         break;
       case "":
         break;
+      case "version":
+        console.log(packageInfo.version);
+        console.log(packageInfo.description);
+        break;
       case "send":
         comm_clnt_command(cmds);
         break;
-      case "prebill":
       case "huidan":
       case "paidan":
       case "test":
@@ -88,7 +56,7 @@ function cli_startup() {
       default:
         console.log(`Unknown command: ${command}`);
         console.log(
-          "Available commands: help,prebill,paidan,huidan,test, loop N,send ,exit"
+          "Available commands: help,paidan,huidan,test, loop N,send ,exit"
         );
         break;
     }
